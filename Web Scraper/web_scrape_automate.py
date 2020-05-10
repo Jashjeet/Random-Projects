3eb4119ca819618d4c8701deee28dfc9c3e5a64d
Jashjeet <53444766+Jashjeet@users.noreply.github.com>
2020-05-10 15:47:47 -0400

    Add files via upload

---
+++ Web Scraper/web_scrape_automate.py
@@ -0,0 +1,87 @@
+
+
+import requests, re
+from bs4 import BeautifulSoup
+from googlesearch import search 
+import pandas as pd
+from tqdm import tqdm
+
+df=pd.read_excel("FY.xlsx", sheet_name='Desi Companies (Source Sheet)')
+l=[]
+
+for emp in tqdm(df['Desi Companies']): 
+    
+    for j in search(emp+" myvisajob",num=1,stop=1):
+
+        if 'myvisajobs.com' in j:
+            r=requests.get(j)
+            c=r.content
+
+            ## Web scraper
+            soup=BeautifulSoup(c,"html.parser")
+
+            try:
+
+                all=soup.find_all("table",{"class":"tbl"})[0]
+
+                var=all.find_all("td")
+                
+                H1B_Dependent=soup.find_all("td",{"align":"right","valign":"top"},text="H1B Dependent:")[0].findNextSiblings()
+
+                Yes_No=H1B_Dependent[0].contents[0][:-2]
+                
+                Citizenship=soup.find_all("td",{"align":"right","valign":"top"},text="Citizenship:")[0].findNextSiblings()
+                
+                Country=Citizenship[0].contents[0]
+
+                x=[(lambda x: x.text)(x) for x in var][:-2]
+
+                length=len(x)
+
+                counter=length//5
+                
+                for i in range(1,counter):
+
+                    dict={}
+                    dict['Employer']=emp
+                    dict['Contact(edit)']=x[i*5]
+                    dict['Name']=x[i*5 + 1]
+                    dict['Job Title']=x[i*5 + 2]
+                    dict['Phone']=x[i*5 + 3]
+                    dict['Email']=x[i*5 + 4]
+                    dict['H1B Dependent']=Yes_No
+                    dict['Country']=Country
+
+                    l.append(dict)
+
+            except:
+                dict={}
+                dict['Employer']=emp
+                dict['Contact(edit)']='-'
+                dict['Name']='-'
+                dict['Job Title']='-'
+                dict['Phone']='-'
+                dict['Email']='-'
+                dict['H1B Dependent']='-'
+                dict['Country']='-'
+
+                l.append(dict)
+            
+        else:
+            dict={}
+            dict['Employer']=emp
+            dict['Contact(edit)']='-'
+            dict['Name']='-'
+            dict['Job Title']='-'
+            dict['Phone']='-'
+            dict['Email']='-'
+            dict['H1B Dependent']='-'
+            dict['Country']='-'
+
+            l.append(dict)
+        
+
+
+df1=pd.DataFrame(l)
+df1.to_csv('final.csv',index=None)
+
